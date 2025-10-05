94980a8e332a95cbec246f71641b8ca1eefbec72
KrashMello <joelenrikesoteldo@gmail.com>
2025-10-05 12:33:30 -0400

    feat: update speedinsights

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <template>
   <Analytics />
+  <SpeedInsights />
   <NuxtLayout />
 </template>
 <script setup>
 import { Analytics } from '@vercel/analytics/nuxt';
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
