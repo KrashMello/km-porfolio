5f04d65634c7c57587048e75693b015f8733e06c
KrashMello <joelenrikesoteldo@gmail.com>
2025-10-05 12:17:17 -0400

    feat: update actions select lang

--- app/components/selectLang.vue
+++ app/components/selectLang.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="group/select relative">
-    <input
-readOnly :value="lang"
-      class="w-9 bg-transparent ring-0 outline-none focus:ring-slate-700 px-1 cursor-pointer font-bold">
-    <ul class="hidden group-focus-within/select:flex flex-col absolute  gap-1 top-6 font-bold">
-      <li class="hover:bg-slate-500 hover:text-white px-1">
-        <button @click="useStateMachineStore().changeLang('es')">es</button>
-      </li>
-      <li class="hover:bg-slate-500 hover:text-white px-1">
-        <button @click="useStateMachineStore().changeLang('en')">en</button>
-      </li>
-    </ul>
+    <button
+class="w-9 bg-transparent ring-0 outline-none focus:ring-slate-700 px-1 cursor-pointer font-bold"
+      @click="handleShow()">{{ lang
+      }}</button>
+    <div :class="`${show ? 'flex' : 'hidden'} flex-col absolute  gap-1 top-6 font-bold`">
+      <button
+class="hover:bg-slate-500 hover:text-white size-12 md:size-7 px-1"
+        @click="() => { useStateMachineStore().changeLang('es'); show = false }">
+        es
+      </button>
+      <button
+class="hover:bg-slate-500 hover:text-white px-1 size-12 md:size-7"
+        @click="() => { useStateMachineStore().changeLang('en'); show = false }">
+        en
+      </button>
+    </div>
   </div>
 </template>
 <script setup>
 const lang = computed(() => useStateMachineStore().lang)
+const show = ref(false)
+const handleShow = () => {
+  show.value = true
+}
 </script>
