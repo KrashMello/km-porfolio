8ea7a190e4c48d27baef23bd7a27a4b882ba3197
KrashMello <joelenrikesoteldo@gmail.com>
2025-10-05 12:16:33 -0400

    feat: update icons

---
+++ app/components/icons/case.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none">
+    <path fill="currentColor" d="M3 20V7h6V4h6v3h6v13H3Zm7-13h4V5h-4v2Z" />
+  </svg>
+</template>
