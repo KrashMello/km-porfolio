b0ae66d6b35fd8650586e442ab89bff2877fb276
KrashMello <joelenrikesoteldo@gmail.com>
2025-10-11 09:12:25 -0400

    feat: update href projects

--- app/components/ui/projects.vue
+++ app/components/ui/projects.vue
@@ -1,6 +1,5 @@
 <template>
-  <section
-id="projects"
+  <section id="projects"
     class="snap-start w-full min-h-screen h-fit flex px-5 md:px-20 py-32 flex-col justify-center md:justify-start items-start gap-6">
     <header class="w-full max-w-7xl mx-auto h-12 md:h-24 md:py-2 justify-start items-start gap-1">
       <h2 class=" flex items-center gap-4 text-[#5E81AC] dark:text-[#88C0D0] text-3xl md:text-6xl font-normal">
@@ -11,14 +10,12 @@ id="projects"
       </h2>
     </header>
     <div class="flex flex-wrap gap-5 max-w-7xl w-full mx-auto">
-      <km-article
-v-for="repo in repoData" :key="repo.id" :title="repo.name"
-        :href="repo.homepage !== '' ? repo.homepage : repo.html_url" :description="repo.description">
+      <km-article v-for="repo in repoData" :key="repo.id" :title="repo.name"
+        :href="!!repo.homepage ? repo.homepage : repo.html_url" :description="repo.description">
         <template #footer>
           <div class="w-full flex text-slate-200 items-center justify-between gap-2.5 text-xs">
             <div class="flex gap-2.5">
-              <span
-:class="`${repo.language == 'TypeScript' ? 'bg-blue-700' : repo.language === 'Vue' ? 'bg-green-500' :
+              <span :class="`${repo.language == 'TypeScript' ? 'bg-blue-700' : repo.language === 'Vue' ? 'bg-green-500' :
                 repo.language === 'HTML' ? 'bg-orange-500' : 'bg-yellow-400'} flex w-3 h-3 rounded-full`" />
               <span class="font-bold text-zinc-600 dark:text-gray-100">
                 {{ repo.language }}
@@ -44,8 +41,7 @@ v-for="repo in repoData" :key="repo.id" :title="repo.name"
       </h2>
     </div>
     <div class="flex max-w-7xl mx-auto flex-wrap gap-5 w-full">
-      <km-article
-title="Kia Landing" href="https://kia-page.vercel.app/kiaSuscripcion"
+      <km-article title="Kia Landing" href="https://kia-page.vercel.app/kiaSuscripcion"
         description="Landing de suscripcion kia motors ecuador">
         <template #footer>
           <div class="w-full flex text-slate-200 items-center justify-between gap-2.5 text-xs">
@@ -61,14 +57,12 @@ title="Kia Landing" href="https://kia-page.vercel.app/kiaSuscripcion"
           </div>
         </template>
       </km-article>
-      <km-article
-v-for="repo in allRepoData" :key="`all-${repo.id}`" :title="repo.name"
-        :href="repo.homepage != null ? repo.homepage : repo.html_url" :description="repo.description">
+      <km-article v-for="repo in allRepoData" :key="`all-${repo.id}`" :title="repo.name"
+        :href="!!repo.homepage ? repo.homepage : repo.html_url" :description="repo.description">
         <template #footer>
           <div class="w-full flex text-slate-200 items-center justify-between gap-2.5 text-xs">
             <div class="flex gap-2.5">
-              <span
-:class="`${repo.language === 'TypeScript' ? 'bg-blue-700' : repo.language === 'Vue' ? 'bg-green-500' :
+              <span :class="`${repo.language === 'TypeScript' ? 'bg-blue-700' : repo.language === 'Vue' ? 'bg-green-500' :
                 repo.language === 'HTML' ? 'bg-orange-500' : 'bg-yellow-400'} flex w-3 h-3 rounded-full`" />
               <span class="font-bold text-zinc-600 dark:text-gray-100">
                 {{ repo.language }}
